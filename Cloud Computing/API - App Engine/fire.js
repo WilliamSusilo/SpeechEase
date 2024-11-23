aa43d91292790086a743df8ffc9f36625d67ef49
Ivander Kendrick Wijono <ken100840@gmail.com>
2024-11-23 19:59:31 +0700

    Make the Register & Login Feature

--- Cloud Computing/API - App Engine/fire.js
+++ Cloud Computing/API - App Engine/fire.js
@@ -1,30 +1,15 @@
-const { initializeApp } = require('firebase/app');
-const { getFirestore } = require('firebase/firestore');
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBcpwf11y017vYO--lkEun2-gwY9JprHFA",
-  authDomain: "speechease-iw10810.firebaseapp.com",
-  projectId: "speechease-iw10810",
-  storageBucket: "speechease-iw10810.firebasestorage.app",
-  messagingSenderId: "950424434728",
-  appId: "1:950424434728:web:aa2452f2bbc439a4969d1d",
-  measurementId: "G-7Y6TTN3674"
-};
+// Inisialisasi Firebase dengan menggunakan kredensial service account
+const serviceAccount = require("./serviceAccountKey.json");
 
-// Initialize Firebase App
-const app = initializeApp(firebaseConfig);
+initializeApp({
+  credential: cert(serviceAccount), // gunakan kredensial service account
+});
 
-// Initialize Firestore
-const db = getFirestore(app);
-
-// Initialize Firebase Admin SDK
-const serviceAccount = require("./serviceAccountKey.json"); 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
-  });
-}
+// Inisialisasi Firestore
+const db = getFirestore();
 
 module.exports = { db, admin };
