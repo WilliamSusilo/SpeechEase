af450cfcbd2441e7897540a8a5010df18107de9a
William <williamsusilo810@gmail.com>
2024-11-24 01:12:57 +0700

    First init for Flask API to consume ML Model

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ import tensorflow_hub as hub
 #For testing = http://127.0.0.1:5000/predict
 app = Flask(__name__)
 
-# Load the model
+#Load the model
 model = tf.keras.models.load_model('./model/speech_classification.h5')
 
-# Initialize YAMNet layer
+#Initialize YAMNet layer
 yamnet_url = "https://tfhub.dev/google/yamnet/1"
 yamnet_layer = hub.KerasLayer(yamnet_url, trainable=False)
 
-# Load audio files and process waveform
+#Load audio files and process waveform
 def load_audio_waveform(audio_path, sr=16000):
     try:
         waveform, _ = librosa.load(audio_path, sr=sr, mono=True)
@@ -38,31 +38,31 @@ def extract_yamnet_features(waveform, sr=16000):
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # if 'file' not in request.files:
-    #     return jsonify({'error': 'No file uploaded'}), 400
+    #Key for testing must be filled with = file
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
     
-    # Get file from request
+    #Get file from request
     file = request.files['file']
     
-    # Save file temporarily
+    #Save file temporarily
     file_path = f"{file.filename}"
     file.save(file_path)
 
     try:
-        ### Data pre-processing
-        # Load the audio file and preprocess it
+        #Load the audio file and preprocess it
         waveform = load_audio_waveform(file_path)
 
-        # Extract YAMNet features
+        #Extract YAMNet features
         features = extract_yamnet_features(waveform)
 
-        # Prepare input for model
-        input_data = np.expand_dims(features, axis=0)  # Adjust dimensions for prediction
+        #Prepare input for model (adjust dimensions for prediction)
+        input_data = np.expand_dims(features, axis=0) 
 
-        # Predict using the trained model
-        predictions = model.predict(input_data)[0][0]  # Dapatkan probabilitas prediksi
+        #Predict using the trained model (get the probability)
+        predictions = model.predict(input_data)[0][0] 
 
-        # Map prediction to class
+        #Map prediction to class
         if predictions >= 0.5:
             result = "Masih Salah, Coba Lagi"
         else:
